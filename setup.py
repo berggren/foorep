253c99a9ae0151c9f782671e7e1ddff6594adba6
Johan Berggren <jbn@klutt.se>
2012-12-21 09:05:46 +0100

    Fix in templates. Bugfix release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='foorep',
-    version='0.1.4',
+    version='0.1.4-1',
     author='Johan Berggren',
     author_email='jbn@klutt.se',
     packages=['foorep', 'foorep.test'],
     url='http://foorensics.blogspot.com',
     license='LICENSE.txt',
     description='Malware Repository for humans',
-    long_description=open('README.md').read(),
+    long_description='Malware Repository for humans',
     install_requires=[
         "pymongo==2.4",
         "CherryPy==3.2.2",
